ad8b6761d437aa7fd2b65aa20d7dc3679a2557bb
Will <w_birdseye@hotmail.co.uk>
2021-11-16 14:38:24 +0000

    Setup database for makersbnb

---
+++ db/migrations/01_database_makersbnb.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE makersbnb;
+CREATE DATABASE makersbnb_test;
+
+CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(300), price FLOAT, email VARCHAR(50));
+INSERT INTO "public"."spaces" ("id", "name", "description", "price", "email") VALUES (1, '10 downing street', 'One room flat, suitable for head of state', 10, 'example@test.com');
