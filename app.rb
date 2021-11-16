86e7f2e6213de4b94ba73f61cb661e2eaadc91e3
Blessing <blessing@Blessings-Air.broadband>
2021-11-16 11:23:47 +0000

    added a feature test for viewing spaces on the site and passed the feature test

--- app.rb
+++ app.rb
@@ -11,5 +11,9 @@ class MakersBnB < Sinatra::Base
     erb :index
   end
 
+  get '/spaces' do 
+    erb :spaces
+  end 
+
   run! if app_file == $0
 end
---
+++ spec/features/view_spaces_spec.rb
@@ -0,0 +1,10 @@
+feature "User can see list of spaces available" do 
+    scenario "Shows a list of spaces" do 
+        visit("/")
+        click_link('View spaces')
+        expect(page).to have_content('Name: 10 Downing street')
+        expect(page).to have_content('Description: One room flat, suitable for head of state')
+        expect(page).to have_content('Price per night: 10')
+        expect(page).to have_content('example@test.com')
+    end 
+end 
