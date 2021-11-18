7a05f4a727e96a60638af186550584ad72a53647
Blessing <blessing@Blessings-Air.broadband>
2021-11-18 15:47:47 +0000

    written feature test so that user can check booking for their spaces User can find a list of bookings in their my-spaces page

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,5 +1,6 @@
 require 'pg'
 
+
 class Booking
 
   attr_reader :date, :space_id
@@ -16,5 +17,17 @@ class Booking
     result = connection.exec_params('INSERT INTO bookings (date, approval_status, account_id, space_id) VALUES($1, $2, $3, $4) RETURNING id, date, approval_status, account_id, space_id;', [date, approval_status, account_id, space_id])
     Booking.new(date: result[0]['date'], approval_status: result[0]['approval_status'], account_id: result[0]['account_id'], space_id: result[0]['space_id'])
   end
+
+  def self.get_space_booking(space_id:)
+    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'makersbnb_test') : connection = PG.connect(dbname: 'makersbnb')
+    result = connection.exec_params('SELECT * FROM bookings WHERE space_id = $1 AND approval_status = pending;', [space_id])
+    result.map do |booking|
+      Booking.new(date: booking['date'], approval_status: booking['approval_status'], account_id: booking['account_id'], space_id: booking['space_id'])
+    end
+  end 
   
+end
+
+result.map do |space|
+  Space.new(name: space['name'], description: space['description'], price: space['price'], email: space['email'])
 end
---
+++ spec/features/viewing_bookings_in_my_spaces_page_spec.rb
@@ -0,0 +1,22 @@
+feature "Logged in user can check booking for their spaces" do 
+    scenario "User can find a list of bookings in their my-spaces page" do 
+    login_steps
+    user2 = Account.create(name: "Johnny", email: "johnnybravo@example.com", password: "test4")
+    visit("/")
+    click_link('View spaces')
+    click_button('Book a night')
+    fill_in "date", with: "2025-01-01"
+    click_button('Book')
+    visit('/login')
+    fill_in('email', with: 'Bigmoneybob@hotmail.com')
+    fill_in('password', with: 'test')
+    click_button('Log in')
+    click_link('My spaces')
+
+    expect(page).to have_content('johnnybravo@example.com')
+    expect(page).to have_content('Name: Las vegas')
+    expect(page).to have_content('Description: Close to all the bars and casinos on the strip')
+    expect(page).to have_content('Price: 999')
+    expect(page.status_code).to be 200
+    end 
+end 
